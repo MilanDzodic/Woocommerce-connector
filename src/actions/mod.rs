9c1924c129a14cf0773319373ed6dfd34437f2c1
MilanDzodic <milan_dzodic@hotmail.com>
2026-02-03 14:51:04 +0100

    Revert "fixat list_all_products action och gjort tester för att hämta produkt med sku"

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -9,9 +9,5 @@ pub mod retrieve_customer_by_id {
     include!("../actions/retrieve_customer_by_id/action.rs");
 }
 
-pub mod search_products_by_sku {
-    include!("../actions/search_products_by_sku/action.rs");
-}
-
 
 
