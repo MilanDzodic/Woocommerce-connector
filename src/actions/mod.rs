85440aaab53a4239067e303e488c0fb9c48f322d
Milan Dzodic <milan_dzodic@hotmail.com>
2026-02-23 12:10:15 +0100

    removed irrelevant actions

--- src/actions/mod.rs
+++ src/actions/mod.rs
@@ -13,24 +13,8 @@ pub mod retrieve_customer_by_id {
     include!("../actions/retrieve_customer_by_id/action.rs");
 }
 
-pub mod retrieve_product_by_id {
-    include!("../actions/retrieve_product_by_id/action.rs");
-}
-
 pub mod search_products {
     include!("../actions/search_products/action.rs");
-}
-
-pub mod update_customer_by_id {
-    include!("../actions/update_customer_by_id/action.rs");
-}
-
-pub mod update_order_by_id {
-    include!("../actions/update_order_by_id/action.rs");
-}
-
-pub mod update_product_by_id {
-    include!("../actions/update_product_by_id/action.rs");
 }
 
 
